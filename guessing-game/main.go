c8ba6ab7429f6098e9671e9423002ebb9441db6a
Jake Gameroff <143146145+jakegameroff@users.noreply.github.com>
2026-03-03 15:30:26 -0500

    project 1 done

--- guessing-game/main.go
+++ guessing-game/main.go
@@ -3,21 +3,38 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 )
 
 func startGame() {
+	numGuesses := 0
 	secretNumber := rand.Intn(100) + 1
+	
 	var guess int
     for guess != secretNumber {
 		fmt.Print("What is your guess: ")
-		fmt.Scan(&guess)
+		
+		var input string
+		var err error
+
+		fmt.Scan(&input)
+		guess, err = strconv.Atoi(input)
+
+		if err != nil {
+			fmt.Print("Please enter a number!\n")
+			continue
+		}
+
+		numGuesses += 1
 
 		if guess < secretNumber {
 			fmt.Print("Too low!\n")
 		} else if guess > secretNumber {
 			fmt.Print("Too high!\n")
 		} else {
-			fmt.Printf("Congratulations! Your guess of %d is correct! :)\n", secretNumber)
+			fmt.Printf(
+				"Congratulations! Your guess of %d is correct! It took %d guesses.",
+				secretNumber, numGuesses)
 		}
 	}
 }

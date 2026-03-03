27410113cc814d70c82d9a16bf8917bf00cea6b0
Jake Gameroff <143146145+jakegameroff@users.noreply.github.com>
2026-03-03 15:09:17 -0500

    project 1 done

--- guessing-game/main.go
+++ guessing-game/main.go
@@ -6,9 +6,22 @@ import (
 )
 
 func startGame() {
-	secretNumber := rand.IntN(100) + 1
+	secretNumber := rand.Intn(100) + 1
 	var guess int
     for guess != secretNumber {
+		fmt.Print("What is your guess: ")
+		fmt.Scan(&guess)
 
+		if guess < secretNumber {
+			fmt.Print("Too low!\n")
+		} else if guess > secretNumber {
+			fmt.Print("Too high!\n")
+		} else {
+			fmt.Printf("Congratulations! Your guess of %d is correct! :)\n", secretNumber)
+		}
 	}
+}
+
+func main() {
+	startGame()
 }
